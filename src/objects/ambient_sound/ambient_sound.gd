b7e57c4fb367897b9af40bdc18331c783e42cb1d
RichShasha <richardsasalevich@outlook.com>
2024-07-27 20:11:56 +0300

    Added Amibient Sounds

--- src/objects/ambient_sound/ambient_sound.gd
+++ src/objects/ambient_sound/ambient_sound.gd
@@ -8,7 +8,6 @@ var amobient_sounds: Array[AudioStream] = [preload("res://assets/temporary/1.mp3
 func _on_timer_timeout():
 	random.stream = amobient_sounds.pick_random()
 	random.play()
-	
 
 
 func _on_random_finished():
