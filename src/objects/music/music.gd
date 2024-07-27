8ef1c1f9f9df1040c7b4a6d197507fe7b651848a
unknown <you@example.com>
2024-07-27 18:44:24 +0300

    Catch mechanic added

---
+++ src/objects/music/music.gd
@@ -0,0 +1,30 @@
+extends AudioStreamPlayer2D
+
+@onready var tracks: Array[AudioStream] = [
+	preload("res://assets/temporary/1.mp3"), 
+	preload("res://assets/temporary/2.mp3"), 
+	preload("res://assets/temporary/3.mp3"),
+]
+
+@onready var play_next: Array = range(len(tracks))
+
+func _ready() -> void:
+	randomize()
+	play_next.shuffle()
+	stream = tracks[play_next.pop_front()]
+	play()
+
+
+func _on_finished() -> void:
+	if len(play_next) == 1:
+		var last_track: int = play_next[0]
+		play_next = range(len(tracks))
+		play_next.shuffle()
+		if last_track == play_next[0]:
+			play_next[0] = play_next[-1]
+			play_next[-1] = last_track
+		stream = tracks[last_track]
+		play()
+		return
+	stream = tracks[play_next.pop_front()]
+	play()
--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -2,20 +2,19 @@ extends Node2D
 
 enum STATE {
 	InMenu,
-	Waiting,
 	Throwing,
 	Catching,
 	Pulling,
 	Swimming,
 }
 
-@export var game_state: STATE = STATE.InMenu
+var game_state: STATE = STATE.InMenu
 
 # TODO: add all new objects into dict
 @onready var objects: Dictionary = {
 	"menu": 	%MenuUI,
 	"throw": 	%Throw,
-	"catch": 	null,
+	"catch": 	%Catch,
 	"pull": 	null,
 	"swim": 	%Swim,
 }
@@ -37,6 +36,8 @@ func activate_object(new_state: STATE = game_state) -> void:
 			objects["swim"].activate()
 		STATE.Throwing:
 			objects["throw"].activate()
+		STATE.Catching:
+			objects["catch"].activate()
 		_:
 			assert(false, "Incorrect state/object ot activate")
 
@@ -44,5 +45,5 @@ func activate_object(new_state: STATE = game_state) -> void:
 # TODO: for all new objects add connection
 # Connect functions to finished signals in objects
 func connect_objects() -> void:
-	objects["menu"].finished.connect(activate_object.bind(STATE.Throwing))
+	objects["menu"].finished.connect(activate_object.bind(STATE.Catching))
 	#objects["swim"].finished.connect(finish_swim)
