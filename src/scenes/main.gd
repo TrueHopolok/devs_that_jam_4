1b51d94c15a8125e6508537cd92ab28ee1d1b71e
unknown <you@example.com>
2024-07-27 14:57:52 +0300

    Main script partly done

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,4 +1,44 @@
 extends Node2D
 
+enum STATE {
+	InMenu,
+	Waiting,
+	Throwing,
+	Catching,
+	Pulling,
+	Swimming,
+}
+
+@export var game_state: STATE = STATE.InMenu
+
+# TODO: add all new objects into dict
+var objects: Dictionary = {
+	"menu": 	null,
+	"throw": 	null,
+	"catch": 	null,
+	"pull": 	null,
+	"swim": 	null,
+}
+
+
 func _ready() -> void:
+	connect_objects()
+	activate_object()
+
+
+# TODO: for all new object add activation
+# Activate ceratain object based on game state
+func activate_object() -> void:
+	match game_state:
+		STATE.InMenu:
+			#objects["menu"].activate()
+			pass
+		_:
+			assert(false, "Incorrect state/object ot activate")
+
+
+# TODO: for all new objects add connection
+# Connect functions to finished signals in objects
+func connect_objects() -> void:
+	#objects["menu"].finished.connect(finish_menu)
 	pass
