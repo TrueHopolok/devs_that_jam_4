2046803e53522e18576a8f8db5c81a884cfb8e01
unknown <you@example.com>
2024-07-27 17:04:15 +0300

    Added swim mechanic

--- src/objects/menu/menuUI.gd
+++ src/objects/menu/menuUI.gd
@@ -1,10 +1,19 @@
-extends Control
+extends MarginContainer
 
-@onready var camera : AnimationPlayer = get_tree().get_first_node_in_group("CameraAnimation")
+signal finished
+
+@onready var camera: AnimationPlayer = get_tree().get_first_node_in_group("CameraAnimation")
 
 
-func _ready():
-	pass
+func _ready() -> void:
+	camera.animation_finished.connect(func(anim_name: String) -> void:
+		if anim_name == "zoomout":
+			finished.emit())
 
-func _on_button_pressed():
+
+func _on_button_pressed() -> void:
 	camera.play("zoomout")
+
+
+func activate() -> void:
+	camera.play("default")
