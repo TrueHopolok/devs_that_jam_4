eeba4685e7e47913bc750186af44679e8872ed8f
RichShasha <richardsasalevich@outlook.com>
2024-07-28 03:03:46 +0300

    Added all items for paralax

--- src/objects/paralax/background.gd
+++ src/objects/paralax/background.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 @export var move_curve: Curve
 
-@onready var constant_movement: ParallaxBackground = $ConstantMovement
-@onready var dynamic_movement: ParallaxBackground = $DynamicMovement
+@onready var constant_clouds: ParallaxBackground = $ConstantClouds
+@onready var DynamicBackgrounds: Array = get_tree().get_nodes_in_group("Dynamic_background")
 
 var counter : float = 1
 
 func _process(delta: float) -> void:
-	constant_movement.scroll_base_offset -= Vector2(100, 0) * delta
+	constant_clouds.scroll_base_offset -= Vector2(100, 0) * delta
 	if counter <= 1:
-		dynamic_movement.scroll_base_offset -= Vector2(400 * move_curve.sample(counter), 0) * delta
+		for background in DynamicBackgrounds:
+			background.scroll_base_offset -= Vector2(400 * move_curve.sample(counter), 0) * delta
 		counter += delta
 
 
